e1c6592a1175801eb17bacddd71b95fa626aa8b9
agruenth <adrian.gruenther@gmail.com>
2017-01-06 16:13:07 +0100

    es geht

--- app/src/main/java/com/example/alex/maps_app/MapsActivity.java
+++ app/src/main/java/com/example/alex/maps_app/MapsActivity.java
@@ -24,7 +24,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -43,7 +42,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        mMap.setMyLocationEnabled(true);
+        //mMap.setMyLocationEnabled(true);
 
     }
 }
