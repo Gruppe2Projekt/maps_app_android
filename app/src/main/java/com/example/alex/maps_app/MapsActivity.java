13cf7eb301e6311a2942bbbb93bca9ebdf03c697
Alex <alexrex11@gmail.com>
2017-01-06 14:33:52 +0100

    Standort eingefügt

--- app/src/main/java/com/example/alex/maps_app/MapsActivity.java
+++ app/src/main/java/com/example/alex/maps_app/MapsActivity.java
@@ -43,5 +43,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setMyLocationEnabled(true);
+
     }
 }
